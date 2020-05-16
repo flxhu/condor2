794a99c5c3231a34283ebbdbdc07a2511088982a
Felix Hupfeld <felix@quobyte.com>
2020-05-16 16:56:40 +0200

    Added clipping. Pull more information from .trn

--- condor_obj_file_tool.py
+++ condor_obj_file_tool.py
@@ -8,21 +8,28 @@ import json
 def read_trn(trn_file):
     print("Parsing", trn_file)
     with open(trn_file, "rb") as f:
-        width, height = struct.unpack('ii', f.read(8))  #  8
+        width, height = struct.unpack('ii', f.read(8))
         struct.unpack('fff', f.read(12))  # 12, 3 * 90 Grad floats
-        lon, lat = struct.unpack('ff', f.read(8))  # 8
-        print("Lon:", lon, "Lat:", lat)
-        print("Width:", width / 256, "Height:", height / 256)
+        easting, northing = struct.unpack('ff', f.read(8))
 
-        struct.unpack('HHHH', f.read(4*2))  # 8  (33, 78)
+        utm_zone, null = struct.unpack('HH', f.read(4))  # 4  33
+        struct.unpack('HH', f.read(4))  # 78
+
+        easting_lu = easting - width / 256 * 23090
+        northing_lu = northing + height / 256 * 23090
 
         assert f.tell() == 36
 
         # 256 x 256 shorts per tile
         # print(struct.unpack('H', f.read(2)))
-        return lon, lat
 
-def read_obj(obj_file, lon, lat):
+        print("Bottom right (origin) northing", northing, "easting", easting, "zone", utm_zone)
+        print("Upper left northing", northing_lu, "easting", easting_lu, "zone")
+        print("Width:", width / 256, "tiles, Height:", height / 256, "tiles")
+
+        return easting, northing, utm_zone, easting_lu, northing_lu
+
+def read_obj(obj_file, easting, northing):
     result = []
     with open(obj_file, "rb") as f:
         while True:
@@ -34,25 +41,33 @@ def read_obj(obj_file, lon, lat):
 
             buff = f.read(131)
             name = buff[0:lnam]
-            line = {"x" : lon - posx, "y" : lat + posy, "z" : posz, 
-                           "scale" : dim, "orientation": ori, 
-                           "name" : name.decode("ascii")}
+            line = {"x" : easting - posx, "y" : northing + posy, "z" : posz, 
+                    "scale" : dim, "orientation": ori, 
+                    "name" : name.decode("ascii")}
             print(line, posx, posy)
             result.append(line)
     return result
 
-def write_obj(obj_file, lon, lat, objects):
+def write_obj(obj_file, easting, northing, objects):
     print("Writing", obj_file)
     with open(obj_file, "wb") as f:
         for o in objects:
-            print(lon - o['x'],  o['y'] - lat)
             outdata = struct.pack(
                 'fffffB',
-                lon - o['x'],  o['y'] - lat, o['z'], o['scale'], o['orientation'], 
+                easting - o['x'],  o['y'] - northing, o['z'], o['scale'], o['orientation'], 
                 len(o['name']))
             f.write(outdata)
             f.write(o['name'].encode("ascii"))
             f.write(b'\0' * (131 - len(o['name'])))
+
+def clip(objects, e_rb, n_rb, e_lu, n_lu):
+    result = []
+    for object in objects:
+        x = object['x']
+        y = object['y']
+        if x < e_rb and x > e_lu and y < n_lu and y > n_rb:
+           result.append(object)
+    return result
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -71,6 +86,10 @@ if __name__ == "__main__":
         "-j", "--json", 
         help="JSON object data with absolute coordinates", 
         action="store")
+    parser.add_argument(
+        "--noclip",
+        help="Do not filter out objects outside of landscape bounds when importing", 
+        action="store_true")
     args = parser.parse_args()
 
     if not os.path.exists(args.condor_dir):
@@ -86,16 +105,21 @@ if __name__ == "__main__":
     print("Landscape directory", landscape_dir,
           ". Make sure it is writable by your user, otherwise data may end up in the VirtualStore.")
         
-    lon, lat = read_trn(trn_file)
+    easting, northing, utm_zone, easting_lu, northing_lu = read_trn(trn_file)
     if args.command == "export":
-        objects = read_obj(obj_file, lon, lat)
+        objects = read_obj(obj_file, easting, northing)
         print("Read", len(objects), "objects")
         if args.json:
-            print("writing to", args.json)
+            print("Writing to", args.json)
             with open(args.json, "w") as f:
                 f.write(json.dumps(objects, sort_keys=True, indent=2))
+
     elif args.command == "import":
        with open(args.json, "r") as f:
            objects = json.loads(f.read())
            print("Read", len(objects), "objects from", args.json)
-           write_obj(obj_out_file, lon, lat, objects)
+           object_count = len(objects)
+           if not args.noclip:
+               objects = clip(objects, easting, northing, easting_lu, northing_lu)
+               print("Clipping dropped", object_count - len(objects), "objects")
+           write_obj(obj_out_file, easting, northing, objects)
