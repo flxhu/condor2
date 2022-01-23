6b72dbea86362e09c68a8e6efd186abb6552cbfb
flxhu <50048778+flxhu@users.noreply.github.com>
2022-01-23 13:33:39 +0100

    Improve performance and heuristics
    Use a kdtree for querying nearest neighbor. Improve calculation of angle. Add command line parameters.

--- osm_to_objects.py
+++ osm_to_objects.py
@@ -4,14 +4,16 @@ import json
 import math
 import os.path
 import re
+from ast import literal_eval
 
 # pip install ...
 import requests
 import pyproj
+import scipy.spatial
 
 # Configuration
-BOUNDING_BOX = (50.1, 11.6, 54.8,  15.25)
-UTM_ZONE = 33
+BOUNDING_BOX = (50.0, 11.6, 55.0, 17.0)
+UTM_ZONE=33
 WIND_GENERATOR_NAME = "Eolienne2.c3d" 
 POWER_TOWER_NAME = "Powertower.c3d"
 
@@ -35,7 +37,7 @@ def query_overpass(term, bbox, filename):
     query = get_query(bbox, term)
     print(query)
     if os.path.exists(filename):
-        print(filename, "already exists")
+        print(filename, "already exists, not querying")
         return
 
     r = requests.post(url=URL, data=query)
@@ -43,26 +45,11 @@ def query_overpass(term, bbox, filename):
     with open(filename, "w", encoding='utf-8') as f:
         f.write(r.text)
 
-def find_nearest(osm_data, node):
-    nearest = None
-    dist = 999999999999
-    for other in osm_data:
-        if other == None or other == node:
-            continue
-        lon_dist = node['lon'] - other['lon']
-        lat_dist = node['lat'] - other['lat']
-        d = lon_dist * lon_dist + lat_dist * lat_dist
-        if d < dist and d > 0.0001:
-            dist = d
-            nearest = other
-    return nearest
-
 def angle(v1, v2):
     d = (v2[0] - v1[0], v2[1] - v1[1])
     ang1 = math.atan2(0, 0)
     ang2 = math.atan2(d[1], d[0])
     return (ang1 - ang2) % (2.0 * math.pi)
-
 
 def get_object_name(tags):
     if 'generator:source' in tags and tags['generator:source'] == 'wind':
@@ -70,32 +57,55 @@ def get_object_name(tags):
         if 'height:hub' in tags:
             height = int(re.findall(r'\d+', tags['height:hub'])[0])
             scale = height / 100.0
-            print ("scale", tags['height:hub'], scale)
+            # print ("scale", tags['height:hub'], scale)
         return WIND_GENERATOR_NAME, scale
     elif 'power' in tags and tags['power'] == 'tower':
         return POWER_TOWER_NAME, 1.0
     else:
         raise Exception("Unknown object type " + str(tags))
 
-def convert(filename, chain_orientation):
+def convert(filename, utmzone, chain_orientation):
     result = []
-    projection = pyproj.Proj(proj='utm', zone=UTM_ZONE, ellps='WGS84')
-    with open(filename, "r") as f:
+    projection = pyproj.Proj(proj='utm', zone=utmzone, ellps='WGS84')
+    with open(filename, "r", encoding='utf-8') as f:
         osm_data = json.loads(f.read())['elements']
-        print("Found", len(osm_data), "nodes")
-        for node in osm_data:
-            easting, northing = projection(node['lon'], node['lat'])
+        print("Found", len(osm_data), "nodes in", filename)
 
-            object_name, scale = get_object_name(node['tags'])
+        points = []
+        if chain_orientation:
+            for node in osm_data:
+                utm_coordinate = projection(node['lon'], node['lat'])
+                points.append(utm_coordinate)
+            print("Indexed", len(osm_data), "nodes in", filename)
+            kdtree = scipy.spatial.KDTree(points)
+
+        for node in osm_data:
+            utm_coordinate = projection(node['lon'], node['lat'])
 
             orientation = 0.0
             if chain_orientation:
-                nearest_node = find_nearest(osm_data, node)
-                en, nn = projection(nearest_node['lon'], nearest_node['lat'])
-                orientation = angle((easting, northing), (en, nn))
+                distances, indices = kdtree.query(
+                    utm_coordinate, 
+                    k=[2, 3, 4, 5, 7, 8, 9, 10])
+                while distances[0] < 100 and len(distances) > 2:
+                    # Do not consider points too close, they might be from a
+                    # parallel line
+                    distances = distances[1:]
+                    indices = indices[1:]
+                orientation = angle(utm_coordinate, points[indices[0]])
+                orientation2 = (
+                   (angle(utm_coordinate, points[indices[1]]) + math.pi)
+                      % (2.0 * math.pi))
+                orientation = (orientation + orientation2) / 2
+                
+                # print("n", (easting, northing), distances, neighbor, orientation)
 
-            line = {"x" : easting, "y" : northing, "z" : 0.0, 
-                    "scale" : scale, "orientation": orientation, 
+            object_name, scale = get_object_name(node['tags'])
+            line = {"x" : utm_coordinate[0], 
+                    "y" : utm_coordinate[1],
+                    "z" : 0.0, 
+                    "scale" : scale,
+                    "orientation": orientation, 
                     "name" : object_name}
             result.append(line)
     return result
@@ -103,32 +113,47 @@ def convert(filename, chain_orientation):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--condor-dir", 
-        help="Condor 2 base directory", 
-        default="C:/Program Files/Condor2/")
-    parser.add_argument(
         "--bbox",
         help="Bounding box",
-        default="(50.1, 11.6, 54.8,  15.25)",
+        default=str(BOUNDING_BOX),
         action="store")
     parser.add_argument(
-        "--utm_zone", 
+        "--utmzone",
         help="UTM zone",
-        default="33",
+        default=UTM_ZONE,
         action="store")
+    parser.add_argument("--wind", action='store_true')
+    parser.add_argument("--power", action='store_true')
     parser.add_argument(
-        "-j", "--json", 
+        "-o", "--output", 
         help="JSON object data with absolute coordinates", 
         action="store")
     args = parser.parse_args()
 
-    query_overpass(WIND_GENERATOR_TERM, BOUNDING_BOX, "wind_osm.json")
-    query_overpass(POWER_TOWER_TERM, BOUNDING_BOX, "power_osm.json")
+    bounding_box = literal_eval(args.bbox)
 
-    wobjects = convert("wind_osm.json", False)
-    with open("wind_objects.json", "w") as f:
-        f.write(json.dumps(wobjects, sort_keys=True, indent=2))
+    if args.wind:
+      print(f"Querying overpass for wind with bbox: {bounding_box}")
+      query_overpass(
+          WIND_GENERATOR_TERM,
+          bounding_box, 
+          args.output + "_wind_osm.json")
 
-    pobjects = convert("power_osm.json", True)
-    with open("power_objects.json", "w") as f:
-        f.write(json.dumps(pobjects, sort_keys=True, indent=2))
+    if args.power:
+      print(f"Querying overpass for power with bbox: {bounding_box}")
+      query_overpass(
+          POWER_TOWER_TERM, 
+          bounding_box, 
+          args.output + "_power_osm.json")
+
+    if args.wind:
+        wobjects = convert(
+            args.output + "_wind_osm.json", args.utmzone, False)
+        with open(args.output + "_wind_objects.json", "w") as f:
+            f.write(json.dumps(wobjects, sort_keys=True, indent=2))
+
+    if args.power:
+        pobjects = convert(
+            args.output + "_power_osm.json", args.utmzone, True)
+        with open(args.output + "_power_objects.json", "w") as f:
+            f.write(json.dumps(pobjects, sort_keys=True, indent=2))
